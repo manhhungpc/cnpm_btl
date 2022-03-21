ee4b84a3554d95a4d156069f3e5ee19ef9e3530e
manhhungpc <phamhung2002@gmail.com>
2022-03-21 16:39:44 +0700

    setup routes

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+      {/*Todo*/}
+    </div>
+  );
+}
---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Login() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Login</h1>
+      {/*Todo*/}
+    </div>
+  );
+}
