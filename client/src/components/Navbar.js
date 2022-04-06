576b7e1641764f93cf789135a7f182b9c8a65f78
Pham Van Tien <20021449@vnu.edu.vn>
2022-04-06 21:09:04 +0700

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,31 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+  <AppBar position="static">
+    <div>
+    <Tab
+      label={<p className={styles.logoText}>Helpy</p>}
+      className={styles.homeTab}
+      href="/"
+    />
+    <Tab
+      label={<p className={styles.textTab}>Công việc</p>}
+      className={styles.tab}
+      href="/"
+    />
+    <Tab label={<p className={styles.textTab}>Liên hệ</p>} className={styles.tab} />
+    <Tab label={<p className={styles.textTab}>Về chúng tôi</p>} className={styles.tab} />
+    <Button href="/login" variant="contained" color="primary" style={{ marginRight: "20px", marginLeft: "600px"}}>
+      Đăng nhập
+    </Button>
+    <Button href="/register" variant="contained" color="primary">
+       Đăng ký
+    </Button>
+  </div>  
+  </AppBar>);
 }
