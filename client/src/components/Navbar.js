b21da77cae68543264a2acd28ac2b4943b34db44
Pham Van Tien <20021449@vnu.edu.vn>
2022-04-12 08:50:07 +0700

    tao NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-  <AppBar position="static">
+  <AppBar position="static" style={{ backgroundColor: "#fff", color: "#000", height: "50px" }}>
     <div>
     <Tab
       label={<p className={styles.logoText}>Helpy</p>}
@@ -20,7 +20,7 @@ export default function Navbar() {
     />
     <Tab label={<p className={styles.textTab}>Liên hệ</p>} className={styles.tab} />
     <Tab label={<p className={styles.textTab}>Về chúng tôi</p>} className={styles.tab} />
-    <Button href="/login" variant="contained" color="primary" style={{ marginRight: "20px", marginLeft: "600px"}}>
+    <Button href="/login" variant="contained" color="primary" style={{ marginRight: "20px", marginLeft: "560px"}}>
       Đăng nhập
     </Button>
     <Button href="/register" variant="contained" color="primary">
