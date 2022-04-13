b1e9ab6846f7cceb350d25ad5b8eeeff181ba125
manhhungpc <phamhung2002@gmail.com>
2022-04-13 10:58:46 +0700

    signup API

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,30 +1,27 @@
 import bcrypt from "bcrypt";
 import database from "../utils/db";
+import { validateEmail } from "../utils/validate";
 import { responseError } from "../const/app.const";
 
 const Users = database.Model.userModel;
 
 export const login = async (req, res, next) => {
   if (!req.body.email) {
-    res.status(400).json(responseError("Yêu cầu email!"));
+    return res.status(400).json(responseError("Yêu cầu email!"));
   }
 
   if (!req.body.password) {
-    res.status(400).json(responseError("Yêu cầu mật khẩu!"));
+    return res.status(400).json(responseError("Yêu cầu mật khẩu!"));
   }
 
-  const user = await Users.findOne({
-    where: {
-      email: req.body.email,
-    },
-  });
+  const user = await findUserByEmail(req.body.email);
 
   if (!user) {
-    res.status(404).json(responseError("Người dùng không tồn tại"));
+    return res.status(404).json(responseError("Người dùng không tồn tại"));
   }
 
   if (!(await bcrypt.compare(req.body.password, user.password))) {
-    res.status(400).json(responseError("Mật khẩu không đúng"));
+    return res.status(400).json(responseError("Mật khẩu không đúng"));
   }
 
   const formatData = {
@@ -35,4 +32,52 @@ export const login = async (req, res, next) => {
   req.body = formatData;
 
   next();
+};
+
+export const signup = async (req, res, next) => {
+  if (!req.body.username) {
+    return res.status(400).json(responseError("Yâu cầu tên người dùng"));
+  }
+
+  if (!req.body.email) {
+    return res.status(400).json(responseError("Yêu cầu email!"));
+  }
+  if (!validateEmail(req.body.email)) {
+    return res.status(400).json(responseError("Lỗi email không hợp lệ"));
+  }
+
+  if (!req.body.password) {
+    return res.status(400).json(responseError("Yêu cầu mật khẩu!"));
+  }
+
+  if (!req.body.phone_number) {
+    return res.status(400).json(responseError("Yêu cầu sđt"));
+  }
+
+  if (!req.body.address) {
+    return res.status(400).json(responseError("Yêu cầu địa chỉ!"));
+  }
+
+  if (!req.body.skills) {
+    return res.status(400).json(responseError("Yêu cầu ít nhất 1 kỹ năng!"));
+  }
+
+  const user = await findUserByEmail(req.body.email);
+  console.log(user);
+
+  if (user) {
+    return res.status(400).json(responseError("Đã tồn tại người dùng"));
+  }
+
+  next();
+};
+
+const findUserByEmail = async (email) => {
+  const user = await Users.findOne({
+    where: {
+      email: email,
+    },
+  });
+
+  return user;
 };
