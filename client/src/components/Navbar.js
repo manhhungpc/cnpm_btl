bcfe22d1b5336035bd08571b67b16d5517ee4f1b
manhhungpc <phamhung2002@gmail.com>
2022-04-16 16:42:50 +0700

    fix navbar position

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,26 +6,31 @@ import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-  <AppBar position="static" style={{ backgroundColor: "#fff", color: "#000", height: "50px" }}>
-    <div>
-    <Tab
-      label={<p className={styles.logoText}>Helpy</p>}
-      className={styles.homeTab}
-      href="/"
-    />
-    <Tab
-      label={<p className={styles.textTab}>Công việc</p>}
-      className={styles.tab}
-      href="/"
-    />
-    <Tab label={<p className={styles.textTab}>Liên hệ</p>} className={styles.tab} />
-    <Tab label={<p className={styles.textTab}>Về chúng tôi</p>} className={styles.tab} />
-    <Button href="/login" variant="contained" color="primary" style={{ marginRight: "20px", marginLeft: "700px"}}>
-      Đăng nhập
-    </Button>
-    <Button href="/register" variant="contained" color="primary">
-       Đăng ký
-    </Button>
-  </div>  
-  </AppBar>);
+    <AppBar position="static" className={styles.wrapper}>
+      <div className={styles.grid}>
+        <div>
+          <Tab
+            label={<p className={styles.logoText}>Helpy</p>}
+            className={styles.homeTab}
+            href="/"
+          />
+          <Tab
+            label={<p className={styles.textTab}>Công việc</p>}
+            className={styles.tab}
+            href="/"
+          />
+          <Tab label={<p className={styles.textTab}>Liên hệ</p>} className={styles.tab} />
+          <Tab label={<p className={styles.textTab}>Về chúng tôi</p>} className={styles.tab} />
+        </div>
+        <div>
+          <Button href="/login" variant="contained" color="primary" className={styles.btn}>
+            Đăng nhập
+          </Button>
+          <Button href="/register" variant="contained" color="primary" className={styles.btn}>
+            Đăng ký
+          </Button>
+        </div>
+      </div>
+    </AppBar>
+  );
 }
