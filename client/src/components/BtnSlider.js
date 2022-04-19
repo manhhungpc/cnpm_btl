89fe2ab87b6e81cd633e617862ff93a3451dbdab
SONTUNGARRETT <80802090+McGarrett7@users.noreply.github.com>
2022-04-19 20:48:15 +0700

    Homepage

---
+++ client/src/components/BtnSlider.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/Slider_cover.module.css";
+
+console.log(styles);
+
+export default function BtnSlider({ direction, moveSlide }) {
+  console.log(direction, moveSlide);
+  return (
+    <button
+      onClick={moveSlide}
+      // className={direction === "next" ? "Slider_cover_next__rtj2C" : "Slider_cover_prev__hqL9S"}
+      className={ styles.btn_slide + ' ' + ( direction === "next"  ? styles.next : styles.prev ) }
+    >
+      <img src={direction === "next" ? process.env.PUBLIC_URL+"right_arrow.png" : process.env.PUBLIC_URL+"left_arrow.png"} />
+    </button>
+  );
+} 
