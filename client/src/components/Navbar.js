0f36d0907d3b9f6aa6b389bd35cb0808a0e11c6d
SONTUNGARRETT <80802090+McGarrett7@users.noreply.github.com>
2022-04-19 20:38:03 +0700

    Customizing color

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,10 +6,11 @@ import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-  <AppBar position="static" style={{ backgroundColor: "#fff", color: "#000", height: "50px" }}>
+  <AppBar position="sticky" style={{ backgroundColor: "#2d8ce8", color: "#000", height: "50px" }}>
     <div>
     <Tab
       label={<p className={styles.logoText}>Helpy</p>}
+      // img className={styles.photo1} src={process.env.PUBLIC_URL+"Text.png"} />
       className={styles.homeTab}
       href="/"
     />
@@ -20,11 +21,11 @@ export default function Navbar() {
     />
     <Tab label={<p className={styles.textTab}>Liên hệ</p>} className={styles.tab} />
     <Tab label={<p className={styles.textTab}>Về chúng tôi</p>} className={styles.tab} />
-    <Button href="/login" variant="contained" color="primary" style={{ marginRight: "20px", marginLeft: "700px"}}>
-      Đăng nhập
+    <Button href="/login" variant="contained" color="primary" style={{marginLeft: "54%"}}>
+      <p2 className={styles.button}>Đăng nhập</p2>
     </Button>
-    <Button href="/register" variant="contained" color="primary">
-       Đăng ký
+    <Button href="/register" variant="contained" color="primary" style={{marginLeft: "0.5%"}}>
+    <p2 className={styles.button}>Đăng ký</p2>
     </Button>
   </div>  
   </AppBar>);
